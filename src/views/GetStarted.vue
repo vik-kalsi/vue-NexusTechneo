3c3ab116672f6b9bc0103f210c98c428fd0b9fe6
vik-kalsi <107356027+vik-kalsi@users.noreply.github.com>
2024-05-01 22:16:41 +0100

    Finishing Product View and Get Started pages

--- src/views/GetStarted.vue
+++ src/views/GetStarted.vue
@@ -1,5 +1,13 @@
 <template>
+
     <div class="flex justify-center space-x-8 pt-12">
-        Get Started Page
+        <form>
+            <label for="emailaddress">Email Address:</label><br>
+            <input class="text-black font-bold" type="text" id="emailaddress" name="emailaddress"><br>
+            <label for="password">Password</label><br>
+            <input class="text-black font-bold" type="password" id="password" name="password"><br><br>
+            <input class="border-2 p-1" type="submit" value="Login">
+        </form> 
     </div>
+    
 </template>
--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,12 +1,29 @@
 <template>
   <div class="flex justify-center space-x-8 pt-12">
-    <div class="space-y-6">
-      <div class="border-2 p-3">Product 1</div>
-      <div class="border-2 p-3">Product 2</div>
-      <div class="border-2 p-3">Product 3</div>
-      <div class="border-2 p-3">Product 4</div>
-      <div class="border-2 p-3">Product 5</div>
-      <div class="border-2 p-3">Product 6</div>
+    <div class="space-y-6 w-64 md:w-[600px]">
+      <div class="border-2 p-3 md:text-4xl">Product 1
+        <div class="border-2 p-1 m-1 md:text-3xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae adipisci error quia cum corrupti amet natus tempora vel.</div>
+      </div>
+
+      <div class="border-2 p-3 md:text-4xl">Product 2
+        <div class="border-2 p-1 m-1 md:text-3xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae adipisci error quia cum corrupti amet natus tempora vel.</div>
+      </div>
+
+      <div class="border-2 p-3 md:text-4xl">Product 3
+        <div class="border-2 p-1 m-1 md:text-3xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae adipisci error quia cum corrupti amet natus tempora vel.</div>
+      </div>
+
+      <div class="border-2 p-3 md:text-4xl">Product 4
+        <div class="border-2 p-1 m-1 md:text-3xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae adipisci error quia cum corrupti amet natus tempora vel.</div>
+      </div>
+
+      <div class="border-2 p-3 md:text-4xl">Product 5
+        <div class="border-2 p-1 m-1 md:text-3xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae adipisci error quia cum corrupti amet natus tempora vel.</div>
+      </div>
+      
+      <div class="border-2 p-3 md:text-4xl">Product 6
+        <div class="border-2 p-1 m-1 md:text-3xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae adipisci error quia cum corrupti amet natus tempora vel.</div>
+      </div>
     </div>
   </div>
 </template>
